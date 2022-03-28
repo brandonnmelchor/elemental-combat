4af482029395dcb2ab15d1da76ad888acfb17a93
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-03-28 14:57:57 -0500

    create playround function

--- javascript.js
+++ javascript.js
@@ -10,7 +10,30 @@ function computerPlay() {
   }
 }
 
-function playRound(playerSelection, computerSelection) {}
-
-const playerSelection = prompt("aaaaa");
+const playerSelection = prompt("Let's play a game! Rock, paper, or scissors?").toLowerCase();
 const computerSelection = computerPlay();
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) return "It's a tie!";
+  else if (playerSelection === "rock") {
+    if (computerSelection === "paper") return "You Lose! Paper beats Rock.";
+    else return "You Win! Rock beats Scissors.";
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "scissors") return "You Lose! Scissors beats Paper.";
+    else return "You Win! Paper beats Rock.";
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "rock") return "You Lose! Rock beats Scissors.";
+    else return "You Win! Scissors beats Paper.";
+  } else {
+    return playRound(prompt("Choose again! Rock, paper, or scissors?"), computerSelection);
+  }
+}
+
+// function game() {
+//   const playerScore = 0;
+//   const computerScore = 0;
+
+//   for (let i = 0; i < 5; i++) {
+//     console.log(playRound(playerSelection, computerSelection));
+//   }
+// }
