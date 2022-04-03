aa053e0c86d784c50610ce3da72fca98dbe7b13c
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-04-03 13:07:32 -0500

    update html game elements, add css styles, update javascript

--- javascript.js
+++ javascript.js
@@ -5,11 +5,11 @@ function computerPlay() {
   const randomThree = Math.floor(Math.random() * 3) + 1;
 
   if (randomThree === 1) {
-    return "rock";
+    return "fire";
   } else if (randomThree === 2) {
-    return "paper";
+    return "nature";
   } else {
-    return "scissors";
+    return "water";
   }
 }
 
@@ -17,40 +17,32 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return "It's a tie!";
   } else if (
-    (playerSelection === "rock" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "scissors") ||
-    (playerSelection === "scissors" && computerSelection === "rock")
+    (playerSelection === "fire" && computerSelection === "nature") ||
+    (playerSelection === "nature" && computerSelection === "water") ||
+    (playerSelection === "water" && computerSelection === "fire")
   ) {
     computerScore++;
     return `You lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}.`;
-  } else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "scissors" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "rock")
-  ) {
+  } else {
     playerScore++;
     return `You win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}.`;
-  } else {
-    return playRound(prompt("Invalid choice. Please choose again! Rock, paper, or scissors?"), computerSelection);
   }
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Let's play a game! Rock, paper, or scissors?").toLowerCase();
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
-  }
+// function game() {
+//   for (let i = 0; i < 5; i++) {
+//     const playerSelection = prompt("Let's play a game! fire, nature, or water?").toLowerCase();
+//     const computerSelection = computerPlay();
+//     console.log(playRound(playerSelection, computerSelection));
+//   }
 
-  if (playerScore > computerScore) {
-    console.log(`Winner! Player score: ${playerScore}, Computer score: ${computerScore}`);
-  } else {
-    console.log(`Loser! Player score: ${playerScore}, Computer score: ${computerScore}`);
-  }
-}
+//   if (playerScore > computerScore) {
+//     console.log(`Winner! Player score: ${playerScore}, Computer score: ${computerScore}`);
+//   } else {
+//     console.log(`Loser! Player score: ${playerScore}, Computer score: ${computerScore}`);
+//   }
+// }
 
 function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
-game();
