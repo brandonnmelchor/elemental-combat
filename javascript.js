718bb0734df7c89d7d33313141ed495e4c3baed8
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-04-03 22:16:37 -0500

    rework/update index, styles, javascript

--- javascript.js
+++ javascript.js
@@ -1,13 +1,28 @@
-const roundCounter = document.querySelector("#round-counter");
-const playerLivesCounter = document.querySelector("#player-lives");
-const computerLivesCounter = document.querySelector("#computer-lives");
-const gameAnnouncer = document.querySelector("#game-announcer");
-const gameEndMessage = document.querySelector("#game-end-message");
-const playAgain = document.querySelector("#play-again");
-
 let rounds = 0;
 let playerLives = 5;
 let computerLives = 5;
+
+const roundCounter = document.querySelector("#round-counter");
+const LivesCounter = document.querySelector("#lives");
+const gameAnnouncer = document.querySelector("#game-announcer");
+const playAgain = document.querySelector("#play-again");
+
+const elements = document.querySelectorAll(".player-area .element");
+elements.forEach((element) => element.addEventListener("click", playRound));
+
+function playRound() {
+  const playerElement = this.attributes["id"].nodeValue;
+  const computerElement = computerPlay();
+  // const playerSelected = document.querySelector(`.element[id="${this.attributes["id"].nodeValue}"]`);
+
+  gameAnnouncer.textContent = checkRoundWinner(playerElement, computerElement);
+  roundCounter.textContent = `Round:${++rounds}`;
+  LivesCounter.textContent = `Player Lives: ${playerLives}. Enemy Lives: ${computerLives}.`;
+
+  if (playerLives === 0 || computerLives === 0) {
+    checkGameWinner();
+  }
+}
 
 function computerPlay() {
   const randomThree = Math.floor(Math.random() * 3) + 1;
@@ -25,56 +40,43 @@ function checkRoundWinner(playerElement, computerElement) {
     (playerElement === "nature" && computerElement === "water") ||
     (playerElement === "water" && computerElement === "fire")
   ) {
-    computerLives--;
+    playerLives--;
     return `You lose! ${capitalize(computerElement)} beats ${capitalize(playerElement)}.`;
   } else {
-    playerLives--;
+    computerLives--;
     return `You win! ${capitalize(playerElement)} beats ${capitalize(computerElement)}.`;
-  }
-}
-
-function capitalize(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-const elements = document.querySelectorAll(".player-area .element");
-elements.forEach((element) => element.addEventListener("click", playRound));
-
-function playRound() {
-  const computerElement = computerPlay();
-  const playerElement = this.attributes["id"].nodeValue;
-  const playerSelected = document.querySelector(`.element[id="${this.attributes["id"].nodeValue}"]`);
-
-  gameAnnouncer.textContent = checkRoundWinner(playerElement, computerElement);
-  roundCounter.textContent = `Round:${++rounds}`;
-  playerLivesCounter.textContent = `Player Lives: ${playerLives}`;
-  computerLivesCounter.textContent = `Player Lives: ${computerLives}`;
-
-  if (playerLives === 0 || computerLives === 0) {
-    checkGameWinner();
   }
 }
 
 function checkGameWinner() {
   if (playerLives === 0) {
-    gameEndMessage.textContent = "You lost this battle!";
+    gameAnnouncer.textContent = "You lost this battle!";
   } else {
-    gameEndMessage.textContent = "You won this battle!";
+    gameAnnouncer.textContent = "You won this battle!";
   }
 
-  elements.forEach((element) => element.removeEventListener("click", playRound));
-  playAgain.classList.add("play-again");
   playAgain.textContent = "Play again?";
+  playAgain.classList.add("play-again");
   playAgain.addEventListener("click", resetGame);
+
+  elements.forEach((element) => element.removeEventListener("click", playRound));
 }
 
 function resetGame() {
   rounds = 0;
   playerLives = 5;
   computerLives = 5;
+
   roundCounter.textContent = `Round:${rounds}`;
-  playerLivesCounter.textContent = `Player Lives: ${playerLives}`;
-  computerLivesCounter.textContent = `Player Lives: ${computerLives}`;
+  LivesCounter.textContent = `Player Lives: ${playerLives}. Enemy Lives: ${computerLives}.`;
+  gameAnnouncer.textContent = "Combat Area: Empty";
+  playAgain.textContent = "";
+  playAgain.classList.remove("play-again");
+  playAgain.removeEventListener("click", resetGame);
 
   elements.forEach((element) => element.addEventListener("click", playRound));
+}
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
